e5c4428b760d7db64160d157dac15cbd05bdfba2
matthewdargan <matthewdargan57@gmail.com>
2016-07-22 16:36:20 -0500

    Ammo Dispenser working
    Ammo dispenser now gives the player ammo when used and has a set amount
    of health.

---
+++ gmod gamemode/test/entities/entities/ammo_dispenser/shared.lua
@@ -0,0 +1,12 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+ENT.Category = "Buildings"
+
+ENT.BaseHealth = 20
+
+ENT.PrintName = "Ammo Dispenser"
+ENT.Author = "Matthew and Brandon"
+ENT.Purpose = "Give Ammo"
+ENT.Instructions = "Place and use"
+ENT.Spawnable = true
+ENT.AdminSpawnable = false
--- gmod gamemode/test/gamemode/shared.lua
+++ gmod gamemode/test/gamemode/shared.lua
@@ -2,6 +2,7 @@ GM.Name = "Test Gamemode"
 GM.Author = "brando"
 GM.Email = "wowbob396@gmail.com"
 GM.Website = "N/A"
+DeriveGamemode("sandbox")
 
 function GM:Initialize()
 	-- Do stuff
