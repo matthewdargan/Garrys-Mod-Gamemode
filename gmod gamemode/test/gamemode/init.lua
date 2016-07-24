2522dc13c5491768a36648aa14fba2e6310c8ae3
matthewdargan <matthewdargan57@gmail.com>
2016-07-23 23:48:21 -0500

    10th Commit
    Commented out the GM:PlayerSpawn(ply) function. Added a default player
    loadout function.

--- gmod gamemode/test/gamemode/init.lua
+++ gmod gamemode/test/gamemode/init.lua
@@ -9,50 +9,44 @@ include("concommands.lua")
 -- checks to see if f4 menu is opened or closed
 local open = false
 
-function GM:PlayerSpawn(ply)
+--[[ function GM:PlayerSpawn(ply)
 
-ply:SetGravity(.80)
-ply:SetMaxHealth(100)
-ply:SetWalkSpeed(200)
-ply:SetRunSpeed(400)
-ply:SetArmor(50)
+	ply:SetGravity(.80)
+	ply:SetMaxHealth(100)
+	ply:SetWalkSpeed(200)
+	ply:SetRunSpeed(400)
+	ply:SetArmor(50)
 
-ply:Give("weapon_physcannon")
-ply:Give("weapon_physgun")
-ply:Give("weapon_pistol")
-ply:Give("weapon_smg1")
-ply:Give("weapon_ar2")
+	ply:Give("weapon_physcannon")
+	ply:Give("weapon_physgun")
+	ply:Give("weapon_pistol")
+	ply:Give("weapon_smg1")
+	ply:Give("weapon_ar2")
 
-ply:SetAmmo(200,"weapon_ar2")
-ply:SetAmmo(2,"AR2AltFire")
+	ply:SetAmmo(200,"weapon_ar2")
+	ply:SetAmmo(2,"AR2AltFire")
 
-
-ply:SetupHands()
-
-end
+	ply:SetupHands()
+end --]]
 
 function GM:PlayerInitialSpawn(ply)
-if(ply:GetPData("playerLvl") == nil) then
-	ply:SetNWInt("playerLvl",1)
-
-else
-	ply:SetNWInt("playerLvl",ply:GetPData("playerLvl"))
-
+	if (ply:GetPData("playerLvl") == nil) then
+		ply:SetNWInt("playerLvl",1)
+	else
+		ply:SetNWInt("playerLvl",ply:GetPData("playerLvl"))
 	end
-	if(ply:GetPData("playerExp") == nil) then
-		ply:SetNWInt("playerExp",0)
 
+	if (ply:GetPData("playerExp") == nil) then
+		ply:SetNWInt("playerExp",0)
 	else
 		ply:SetNWInt("playerExp",ply:GetPData("playerExp"))
+	end
 
-		end
 	if(ply:GetPData("playerMoney") == nil) then
-			ply:SetNWInt("playerMoney",0)
-
+		ply:SetNWInt("playerMoney",0)
 	else
-			ply:SetNWInt("playerMoney",ply:GetPData("playerMoney"))
-
-		end
+		ply:SetNWInt("playerMoney",ply:GetPData("playerMoney"))
+	end
 end
 
 function GM:OnNPCKilled(npc, attacker, inflictor)
@@ -64,26 +58,34 @@ function GM:OnNPCKilled(npc, attacker, inflictor)
 
 	attacker:SetNWInt("playerExp",attacker:GetNWInt("playerExp")+10)
 
-
 	checkForLevel(attacker)
 end
 
 function GM:PlayerDeath(victim, inflictor, attacker)
-
 	attacker:SetNWInt("playerMoney", attacker:GetNWInt("playerMoney") + 50)
 
 	attacker:SetNWInt("playerExp",attacker:GetNWInt("playerExp")+10)
 
-
 	checkForLevel(attacker)
 end
 
-function checkForLevel(ply)
-local expToLevel = (ply:GetNWInt("playerLvl")*100)*2
-local curExp = ply:GetNWInt("playerExp")
-local curLvl = ply:GetNWInt("playerLvl")
+-- Default Player Loadout
+function GM:PlayerLoadout(ply)
+	ply:Give("weapon_pistol")
+	ply:Give("weapon_physgun")
+	ply:Give("weapon_physcannon")
 
-	if(curExp>=expToLevel)then
+	ply:GiveAmmo(9999, "Pistol", true)
+
+	return true
+end
+
+function checkForLevel(ply)
+	local expToLevel = (ply:GetNWInt("playerLvl")*100)*2
+	local curExp = ply:GetNWInt("playerExp")
+	local curLvl = ply:GetNWInt("playerLvl")
+
+	if (curExp >= expToLevel) then
 	curExp = curExp - expToLevel
 
 	ply:SetNWInt("playerExp", curExp)
@@ -93,16 +95,17 @@ local curLvl = ply:GetNWInt("playerLvl")
 end
 
 util.AddNetworkString("FMenu")
-function GM:ShowSpare2(ply)
-if(open==false) then
-	open = true
-else
-open = false
-end
 
-net.Start("FMenu") -- When F4 key is pressed, it is initialized
+function GM:ShowSpare2(ply)
+	if(open == false) then
+		open = true
+	else
+		open = false
+	end
+
+	net.Start("FMenu") -- When F4 key is pressed, it is initialized
 	net.WriteBit(open)
-net.Broadcast()
+	net.Broadcast()
 end
 
 function GM:PlayerDisconnected(ply)
@@ -114,11 +117,10 @@ function GM:PlayerDisconnected(ply)
 end
 
 function GM:ShutDown()
-
-for k,v in pairs(player.GetAll()) do
-	v:SetPData("playerLvl",v:GetNWInt("playerLvl"))
-	v:SetPData("playerExp",v:GetNWInt("playerExp"))
-	v:SetPData("playerMoney",v:GetNWInt("playerMoney"))
+	for k,v in pairs(player.GetAll()) do
+		v:SetPData("playerLvl",v:GetNWInt("playerLvl"))
+		v:SetPData("playerExp",v:GetNWInt("playerExp"))
+		v:SetPData("playerMoney",v:GetNWInt("playerMoney"))
 	end
 end
 
