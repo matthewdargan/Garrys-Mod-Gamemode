0f3d7abba42dba079bc170de68515001e233bfbf
matthewdargan <matthewdargan57@gmail.com>
2016-07-25 17:40:13 -0500

    13th Commit
    Added barricade entity, have not yet initialized it.

---
+++ gmod gamemode/test/entities/entities/barricade/shared.lua
@@ -0,0 +1,15 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+ENT.Category = "Buildings"
+
+ENT.BaseHealth = 50
+ENT.Cost = 100
+ENT.Owner = nil
+ENT.Model = "models/props_lab/blastdoor001b.mdl"
+
+ENT.PrintName = "Barricade"
+ENT.Author = "Matthew and Brandon"
+ENT.Purpose = "Barricade off doors"
+ENT.Instructions = "Place"
+ENT.Spawnable = true
+ENT.AdminSpawnable = false
